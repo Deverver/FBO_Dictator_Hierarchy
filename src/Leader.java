720a488d264a4da2baf867ce85edc0c375802286
Deverver <jackRMrasmussen@gmail.com>
2024-11-05 16:37:29 +0100

    Initial commit

---
+++ src/Leader.java
@@ -0,0 +1,15 @@
+public abstract class Dictator {
+    private String dictatorName;
+    private int dictatorAge;
+    private int yearsOfDictatorship;
+    private String rulerType;
+    private String rulerKingdom;
+
+
+    abstract void giveSpeech();
+
+    final void printDetails() {
+
+    }
+
+}
